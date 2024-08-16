63268b4b7a69c0f649985333d363fb31b7c70e81
Kyle Stemen <bluelightning32@gmail.com>
2024-08-15 23:35:29 -0700

    Add MatchResolver to track resolving the match rules
    Add the initially functionality to accelerate matching wildcards

---
+++ src/MatchResolver.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+
+using Vintagestory.API.Common;
+using Vintagestory.API.Util;
+
+namespace Gourmand;
+
+/// <summary>
+/// Temporarily holds the state necessary to resolve the matchers. This is
+/// destructed after the matchers are resolved.
+/// </summary>
+public class MatchResolver {
+  private Dictionary<string, List<Item>> _itemVariantsByPrefix;
+  private Dictionary<string, List<Block>> _blockVariantsByPrefix;
+  public readonly IWorldAccessor Resolver;
+
+  // An dictionary of all item variants, indexed by the first part of their code
+  // (everything up to the first '-'). Only items that have a '-' in their code
+  // are included in the dictionary.
+  private IReadOnlyDictionary<string, List<Item>> ItemVariantsByPrefix {
+    get {
+      if (_itemVariantsByPrefix == null) {
+        _itemVariantsByPrefix = new();
+        foreach (Item item in Resolver.Items) {
+          int variantStart = item.Code.Path.IndexOf('-');
+          if (variantStart != -1) {
+            string first = item.Code.Path[..variantStart];
+            if (!_itemVariantsByPrefix.TryGetValue(first,
+                                                   out List<Item> items)) {
+              items = new();
+              _itemVariantsByPrefix.Add(first, items);
+            }
+            items.Add(item);
+          }
+        }
+      }
+      return _itemVariantsByPrefix;
+    }
+  }
+
+  // An dictionary of all block variants, indexed by the first part of their
+  // code (everything up to the first '-'). Only items that have a '-' in their
+  // code are included in the dictionary.
+  private IReadOnlyDictionary<string, List<Block>> BlockVariantsByPrefix {
+    get {
+      if (_blockVariantsByPrefix == null) {
+        _blockVariantsByPrefix = new();
+        foreach (Block block in Resolver.Blocks) {
+          int variantStart = block.Code.Path.IndexOf('-');
+          if (variantStart != -1) {
+            string first = block.Code.Path[..variantStart];
+            if (!_blockVariantsByPrefix.TryGetValue(first,
+                                                    out List<Block> blocks)) {
+              blocks = new();
+              _blockVariantsByPrefix.Add(first, blocks);
+            }
+            blocks.Add(block);
+          }
+        }
+      }
+      return _blockVariantsByPrefix;
+    }
+  }
+
+  public MatchResolver(IWorldAccessor resolver) { Resolver = resolver; }
+
+  private static IReadOnlyList<X> GetMatchingCollectibles<X>(
+      AssetLocation wildcard,
+      System.Func<string, AssetLocation, IReadOnlyList<X>> acceleratedSearch,
+      System.Func<AssetLocation, IReadOnlyList<X>> search,
+      System.Func<AssetLocation, IReadOnlyList<X>> getDirect)
+      where X : CollectibleObject {
+    if (wildcard.Path[0] == '@') {
+      // Everything after the @ is a regular expression.
+      for (int pos = 1; pos < wildcard.Path.Length; ++pos) {
+        if (wildcard.Path[pos] == '-') {
+          // Found the hyphen before any special characters. This can be
+          // accelerated.
+          return acceleratedSearch(wildcard.Path[1..pos], wildcard);
+        }
+        if (!char.IsAsciiLetterOrDigit(wildcard.Path[pos])) {
+          // A special character was found before the hyphen. Use the
+          // unaccelerated search.
+          return search(wildcard);
+        }
+      }
+      // No hyphen or special character was in the regex. The regex was
+      // unnecessary.
+      return getDirect(new AssetLocation(wildcard.Domain, wildcard.Path[1..]));
+    }
+    for (int pos = 0; pos < wildcard.Path.Length; ++pos) {
+      if (wildcard.Path[pos] == '-') {
+        // Found the hyphen before any special characters.
+        for (int starPos = pos + 1; starPos < wildcard.Path.Length; ++starPos) {
+          if (wildcard.Path[starPos] == '*') {
+            // There is a star, and this can be accelerated.
+            return acceleratedSearch(wildcard.Path[0..pos], wildcard);
+          }
+        }
+        // The path is not a wildcard. Use the game's default dictionary.
+        return getDirect(wildcard);
+      }
+      if (wildcard.Path[pos] == '*') {
+        // The asterisk came before the hyphen. This cannot be accelerated.
+        return search(wildcard);
+      }
+    }
+    // The path was not a wildcard.
+    return getDirect(wildcard);
+  }
+
+  private IReadOnlyList<Item>
+  GetMatchingItemsWithoutWildcard(AssetLocation code) {
+    Item item = Resolver.GetItem(code);
+    if (item == null) {
+      return Array.Empty<Item>();
+    } else {
+      return new Item[] { item };
+    }
+  }
+
+  private IReadOnlyList<Item>
+  AcceleratedGetMatchingItems(string firstPart, AssetLocation wildcard) {
+    if (!ItemVariantsByPrefix.TryGetValue(firstPart, out List<Item> search)) {
+      return Array.Empty<Item>();
+    }
+    List<Item> result = new();
+    foreach (Item item in search) {
+      if (WildcardUtil.Match(wildcard, item.Code)) {
+        result.Add(item);
+      }
+    }
+    return result;
+  }
+
+  public IReadOnlyList<Item> GetMatchingItems(AssetLocation wildcard) {
+    return GetMatchingCollectibles(wildcard, AcceleratedGetMatchingItems,
+                                   Resolver.SearchItems,
+                                   GetMatchingItemsWithoutWildcard);
+  }
+
+  private IReadOnlyList<Block>
+  GetMatchingBlocksWithoutWildcard(AssetLocation code) {
+    Block block = Resolver.GetBlock(code);
+    if (block == null) {
+      return Array.Empty<Block>();
+    } else {
+      return new Block[] { block };
+    }
+  }
+
+  private IReadOnlyList<Block>
+  AcceleratedGetMatchingBlocks(string firstPart, AssetLocation wildcard) {
+    if (!BlockVariantsByPrefix.TryGetValue(firstPart, out List<Block> search)) {
+      return Array.Empty<Block>();
+    }
+    List<Block> result = new();
+    foreach (Block block in search) {
+      if (WildcardUtil.Match(wildcard, block.Code)) {
+        result.Add(block);
+      }
+    }
+    return result;
+  }
+
+  public IReadOnlyList<Block> GetMatchingBlocks(AssetLocation wildcard) {
+    return GetMatchingCollectibles(wildcard, AcceleratedGetMatchingBlocks,
+                                   Resolver.SearchBlocks,
+                                   GetMatchingBlocksWithoutWildcard);
+  }
+}
---
+++ test/MatchResolverTest.cs
@@ -0,0 +1,84 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+using Vintagestory.API.Common;
+
+namespace Gourmand.Tests;
+
+[TestClass]
+public class MatchResolverTest {
+  private readonly Mock<IWorldAccessor> _mock = new Mock<IWorldAccessor>();
+  private readonly MatchResolver _resolver;
+
+  public MatchResolverTest() {
+    _mock.Setup(x => x.GetItem(It.IsAny<AssetLocation>()))
+        .Returns<AssetLocation>(LoadAssets.Server.GetItem);
+    _mock.Setup(x => x.SearchItems(It.IsAny<AssetLocation>()))
+        .Returns<AssetLocation>(LoadAssets.Server.SearchItems);
+    _mock.SetupGet(x => x.Items).Returns(LoadAssets.Server.World.Items);
+    _mock.Setup(x => x.GetBlock(It.IsAny<AssetLocation>()))
+        .Returns<AssetLocation>(LoadAssets.Server.GetBlock);
+    _mock.Setup(x => x.SearchBlocks(It.IsAny<AssetLocation>()))
+        .Returns<AssetLocation>(LoadAssets.Server.SearchBlocks);
+    _mock.SetupGet(x => x.Blocks).Returns(LoadAssets.Server.World.Blocks);
+
+    _resolver = new(_mock.Object);
+  }
+
+  [TestMethod]
+  public void ResolveUnnecessaryRegexItem() {
+    IReadOnlyList<Item> items = _resolver.GetMatchingItems(
+        new AssetLocation("game", "@fruit-pineapple"));
+    Assert.AreEqual("fruit-pineapple", items[0].Code.Path);
+    _mock.Verify(x => x.SearchItems(It.IsAny<AssetLocation>()), Times.Never);
+  }
+
+  [TestMethod]
+  public void ResolveComplicatedRegexItem() {
+    IReadOnlyList<Item> items =
+        _resolver.GetMatchingItems(new AssetLocation("game", "@frui.*"));
+    Assert.IsTrue(items.Any((item) => item.Code.Path == "fruit-pineapple"));
+    _mock.Verify(x => x.SearchItems(It.IsAny<AssetLocation>()), Times.Once);
+  }
+
+  [TestMethod]
+  public void ResolveAcceleratedWildcardItem() {
+    IReadOnlyList<Item> items =
+        _resolver.GetMatchingItems(new AssetLocation("game", "fruit-*"));
+    Assert.IsTrue(items.Any((item) => item.Code.Path == "fruit-pineapple"));
+    _mock.Verify(x => x.SearchItems(It.IsAny<AssetLocation>()), Times.Never);
+  }
+
+  [TestMethod]
+  public void ResolveUnacceleratedWildcardItem() {
+    IReadOnlyList<Item> items =
+        _resolver.GetMatchingItems(new AssetLocation("game", "frui*"));
+    Assert.IsTrue(items.Any((item) => item.Code.Path == "fruit-pineapple"));
+    _mock.Verify(x => x.SearchItems(It.IsAny<AssetLocation>()), Times.Once);
+  }
+
+  [TestMethod]
+  public void ResolveExactItemWithHyphen() {
+    IReadOnlyList<Item> items = _resolver.GetMatchingItems(
+        new AssetLocation("game", "fruit-pineapple"));
+    Assert.IsTrue(items.All((item) => item.Code.Path == "fruit-pineapple"));
+    _mock.Verify(x => x.GetItem(It.IsAny<AssetLocation>()), Times.Once);
+  }
+
+  [TestMethod]
+  public void ResolveExactItemWithoutHyphen() {
+    IReadOnlyList<Item> items =
+        _resolver.GetMatchingItems(new AssetLocation("game", "firestarter"));
+    Assert.IsTrue(items.All((item) => item.Code.Path == "firestarter"));
+    _mock.Verify(x => x.GetItem(It.IsAny<AssetLocation>()), Times.Once);
+  }
+
+  [TestMethod]
+  public void ResolveAcceleratedWildcardBlock() {
+    IReadOnlyList<Block> blocks =
+        _resolver.GetMatchingBlocks(new AssetLocation("game", "egg-*"));
+    Assert.IsTrue(blocks.Any((block) => block.Code.Path == "egg-chicken-1"));
+    _mock.Verify(x => x.SearchBlocks(It.IsAny<AssetLocation>()), Times.Never);
+  }
+}
